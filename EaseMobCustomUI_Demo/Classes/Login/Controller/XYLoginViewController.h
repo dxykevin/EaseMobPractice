e6e95571973be247ecd25625b11c434f72b8b426
Kevin <duxinyuan@forevercj.com>
2016-07-05 15:28:00 +0800

    初始化

---
+++ EaseMobCustomUI_Demo/Classes/Login/Controller/XYLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYLoginViewController.h
+//  EaseMobCustomUI_Demo
+//
+//  Created by HoldCourt on 16/7/5.
+//  Copyright © 2016年 HoldCourt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYLoginViewController : UIViewController
+
+@end
